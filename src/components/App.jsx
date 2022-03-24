11750f34fa6076c7704a34790660327f0bd6f61f
Asmita Singh <96052563+Asmi1710@users.noreply.github.com>
2022-03-25 02:56:07 +0530

    implementing mapping and props

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,28 @@
 import React from "react";
 import Card from "./Card";
 import contacts from "../contact";
+import Avatar from "./Avatar";
+
+function createCard(contact) {
+  return (
+    <Card
+      id={contact.id}
+      key={contact.id}
+      name={contact.name}
+      img={contact.imgURL}
+      tel={contact.mob}
+      email={contact.email}
+    />
+  );
+}
 
 function App() {
   return (
     <div>
       <h1 className="heading">My Contacts</h1>
-      <Card
+      <Avatar img="https://pbs.twimg.com/profile_images/434680163599536128/aiTln-vH_400x400.jpeg" />
+      {contacts.map(createCard)}
+      {/* <Card
         name={contacts[0].name}
         img={contacts[0].img}
         tel={contacts[0].tel}
@@ -23,7 +39,7 @@ function App() {
         img={contacts[2].img}
         tel={contacts[2].tel}
         email={contacts[2].email}
-      />
+      /> */}
     </div>
   );
 }
--- src/contact.js
+++ src/contact.js
@@ -1,22 +1,25 @@
 const contacts = [
   {
+    id: "1",
     name: "Sabrina",
-    img:
+    imgURL:
       "https://i.pinimg.com/736x/9a/16/8e/9a168eaf9a069eb5121323d471f24063.jpg",
-    tel: "(310)-865-4000",
+    mob: "(310)-865-4000",
     email: "sabrina_carpenter2323@gmail.com"
   },
   {
+    id: "2",
     name: "Tom Ellis",
-    img: "https://pbs.twimg.com/media/E_cXEv8VkAEy5Jm.jpg",
-    tel: "(312)-565-4880",
+    imgURL: "https://pbs.twimg.com/media/E_cXEv8VkAEy5Jm.jpg",
+    mob: "(312)-565-4880",
     email: "tom12ellis@yahoo.com"
   },
   {
+    id: "3",
     name: "Ariana",
-    img:
+    imgURL:
       "https://static01.nyt.com/images/2014/08/24/arts/24GRANDE1/24JPGRANDE1-superJumbo.jpg",
-    tel: "(310)-444-4000",
+    mob: "(310)-444-4000",
     email: "ariana5678@gmail.com"
   }
 ];
