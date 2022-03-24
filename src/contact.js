93eb02a35b62c75476171cdebe76af7cf06b3ee3
Asmita Singh <96052563+Asmi1710@users.noreply.github.com>
2022-03-24 23:50:33 +0530

    Initial commit

---
+++ src/contact.js
@@ -0,0 +1,24 @@
+const contacts = [
+  {
+    name: "Sabrina",
+    img:
+      "https://i.pinimg.com/736x/9a/16/8e/9a168eaf9a069eb5121323d471f24063.jpg",
+    tel: "(310)-865-4000",
+    email: "sabrina_carpenter2323@gmail.com"
+  },
+  {
+    name: "Tom Ellis",
+    img: "https://pbs.twimg.com/media/E_cXEv8VkAEy5Jm.jpg",
+    tel: "(312)-565-4880",
+    email: "tom12ellis@yahoo.com"
+  },
+  {
+    name: "Ariana",
+    img:
+      "https://static01.nyt.com/images/2014/08/24/arts/24GRANDE1/24JPGRANDE1-superJumbo.jpg",
+    tel: "(310)-444-4000",
+    email: "ariana5678@gmail.com"
+  }
+];
+
+export default contacts;
